0ac6bc567780e71937d744a70e0b609e6da7c8f9
Kerby-cpu <charmeskerby@gmail.com>
2024-12-14 13:32:48 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID ===  '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=2mZ2gTIR#6C1Gnbm_MHRT_mhPbNQvRZemkMDpqaUrOgU10fXQYVI' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID ===  '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=P2p1xBRI#tNiH2Y_2zgK_omjF8ANM1CuAXRRVr0qCoLs18PsIRww' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "*Hello , I am alive now!!*" : process.env.ALIVE_MSG,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
